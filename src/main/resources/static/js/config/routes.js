558ab59eed153d3d8096a5cf6077c81a6c613cd4
Mădălin-Cristian COLEZEA <madalin_cristian.colezea@cti.pub.ro>
2015-04-26 15:39:11 +0300

    modificari

--- src/main/resources/static/js/config/routes.js
+++ src/main/resources/static/js/config/routes.js
@@ -1,13 +1,13 @@
 app.config(function($routeProvider, $httpProvider) {
 
     $routeProvider.when('/', {
-        templateUrl : 'views/home.html',
+        templateUrl : 'home.html',
         controller : 'home'
     }).when('/login', {
-        templateUrl : 'views/login.html',
+        templateUrl : 'login.html',
         controller : 'navigation'
     }).when('/account', {
-        templateUrl: 'views/account.html',
+        templateUrl : 'account.html',
         controller: 'account'
     }).otherwise('/');
 
