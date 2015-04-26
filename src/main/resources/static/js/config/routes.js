1fcff416f82349912441da4438f296036ab36afd
vladspartan <vlad_andrey88@yahoo.com>
2015-04-26 18:30:11 +0300

    Login bootstrap

--- src/main/resources/static/js/config/routes.js
+++ src/main/resources/static/js/config/routes.js
@@ -9,6 +9,9 @@ app.config(function($routeProvider, $httpProvider) {
     }).when('/account', {
         templateUrl : 'account.html',
         controller: 'account'
+    }).when('/creearesala', {
+        templateUrl : 'creearesala.html',
+        controller: 'creearesala'
     }).otherwise('/');
 
     $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
